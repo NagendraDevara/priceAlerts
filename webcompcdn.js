452d9dc90d0243fa0aeeed6f7139e01b8e512f9f
Nagendra Devara <nagendradevara4@gmail.com>
2020-08-28 20:49:33 +0530

    Create webcompcdn.js

---
+++ webcompcdn.js
@@ -0,0 +1,22 @@
+et v = "indiacate variable";
+class MyCountry extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `<p> this is a paragraph</p>
+    <p>this is another one ${v}<p>
+<button onclick="hel()">hello</button>`;
+  }
+}
+class Another extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `<p> this is a another paragraph</p>
+    <p>this is another one paragraph ${v}<p>
+<button onclick="hel()">hello</button>`;
+  }
+}
+function hel() {
+  alert("clicked button");
+}
+window.customElements.define("my-india", MyCountry);
+window.customElements.define("my-para", Another);
